0fa3084e3cdd7b19949786398458752803e96b86
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2025-08-10 23:42:49 +0530

    add imageUrl field too

--- app/createBlog/page.tsx
+++ app/createBlog/page.tsx
@@ -6,11 +6,12 @@ import gqlClient from "@/service/gql";
 import { gql } from "graphql-tag";
 const CreateBlogpage = () => {
   const CREATE_BLOG = gql`
-   mutation CreatBlog($title: String!, $content: String!, $authorId: ID!) {
-  createBlog(title: $title, content: $content, authorId: $authorId) {
+  mutation CreatBlog($title: String!, $content: String!, $authorId: ID!, $imageUrl: String) {
+  createBlog(title: $title, content: $content, authorId: $authorId, imageUrl: $imageUrl) {
     title
     content
     authorId
+    imageUrl
   }
 }
   `;
@@ -19,6 +20,7 @@ const CreateBlogpage = () => {
     const blog = await gqlClient.request(CREATE_BLOG, {
       title: title,
       content: content,
+      imageUrl:imageUrl,
       authorId:authorId
     });
     console.log(blog);
@@ -30,6 +32,7 @@ const CreateBlogpage = () => {
   const [title, setTittle] = useState("");
   const [content, setContent] = useState("");
    const [authorId, setAuthorId] = useState("");
+   const [imageUrl,setImageUrl] =  useState("")
   return (
  <div className="min-h-screen flex items-center justify-center bg-black px-4">
   <form
@@ -75,21 +78,39 @@ const CreateBlogpage = () => {
         className="w-full border border-gray-700 bg-gray-800 text-white rounded-lg px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
       ></textarea>
     </div>
-    
+
+      <div>
+      <label
+        htmlFor="image"
+        className="block text-sm font-medium text-gray-300 mb-1"
+      >
+        Image-Url:
+      </label>
+      <input
+        name="image"
+        value={imageUrl}
+        onChange={(e) => setImageUrl(e.target.value)}
+        id="image"
+        type="text"
+        placeholder="Enter a imageUrl"
+        className="w-full border border-gray-700 bg-gray-800 text-white rounded-lg px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+      />
+    </div>
+
     <div>
       <label
-        htmlFor="id"
+        htmlFor="authorid"
         className="block text-sm font-medium text-gray-300 mb-1"
       >
         Author-Id:
       </label>
       <input
-        name="id"
+        name="authorid"
         value={authorId}
         onChange={(e) => setAuthorId(e.target.value)}
-        id="id"
+        id="authorid"
         type="text"
-        placeholder="Enter a title"
+        placeholder="Enter a authorId"
         className="w-full border border-gray-700 bg-gray-800 text-white rounded-lg px-3 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
       />
     </div>
